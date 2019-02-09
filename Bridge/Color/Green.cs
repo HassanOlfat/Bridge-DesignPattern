6e5699c00e22a33da167596c1ac570fca58174a8
hassan olfat <hassan.olfat@LIVE.COM>
2019-02-09 13:12:25 +0330

    ColorsClasses

---
+++ Bridge/Color/Green.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bridge.Color
+{
+    class Green : IColor
+    {
+        public void ApplyColor()
+        {
+          Console.WriteLine("Set Green");
+        }
+    }
+}
---
+++ Bridge/Color/Red.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bridge.Color
+{
+    class Red : IColor
+    {
+        public void ApplyColor()
+        {
+            Console.WriteLine("Set Red");
+        }
+    }
+}
